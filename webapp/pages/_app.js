561dc378657f4843cb1a78c20aaf2d4586401339
Mikhail <pbvby1994@gmail.com>
2025-11-19 15:15:45 +0500

    new

--- webapp/pages/_app.js
+++ webapp/pages/_app.js
@@ -1,19 +1,38 @@
 // Файл: webapp/pages/_app.js
+import dynamic from 'next/dynamic';
 import '../styles/globals.css';
-// УДАЛЕНО: import { Analytics } from "@vercel/analytics/react"; 
+
+// Для аналитики на Vercel (сохраняем, если это было в предыдущей версии)
+import { Analytics } from "@vercel/analytics/react"; 
+
+// Динамический импорт Layout с отключенным SSR. 
+// Layout содержит PlayerProvider и логику Telegram SDK, 
+// поэтому он должен работать только на клиенте.
+const ClientLayout = dynamic(() => import('../components/Layout'), { 
+  ssr: false, 
+  loading: () => (
+    // Загрузочный экран, пока инициализируется SDK/компонент
+    <div className="flex justify-center items-center h-screen text-lg text-txt-primary bg-bg-default">
+        Инициализация WebApp...
+    </div>
+  )
+});
 
 /**
  * Основной компонент приложения Next.js.
  * Обертывает все страницы (Component) и позволяет применять глобальные стили/обертки.
  */
 function MyApp({ Component, pageProps }) {
+  // Весь компонент оборачивается в ClientLayout
   return (
     <>
-      {/* Основной компонент страницы */}
-      <Component {...pageProps} />
+      <ClientLayout>
+        {/* Основной компонент страницы */}
+        <Component {...pageProps} />
+      </ClientLayout>
       
-      {/* УДАЛЕНО: Интеграция Vercel Analytics */}
-      {/* <Analytics /> */}
+      {/* Интеграция Vercel Analytics */}
+      <Analytics />
     </>
   );
 }
