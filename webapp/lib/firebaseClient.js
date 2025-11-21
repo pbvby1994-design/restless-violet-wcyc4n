5e42e8428a2d3c5659cc2d508e9844e1200307a6
Mikhail <pbvby1994@gmail.com>
2025-11-21 09:53:20 +0500

    Update firebaseClient.js

--- webapp/lib/firebaseClient.js
+++ webapp/lib/firebaseClient.js
@@ -5,10 +5,8 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-// ⚠️ Предполагается, что вы определили эти переменные окружения
-// в настройках Vercel или в .env.local
-
 const firebaseConfig = {
+    // Используйте переменные окружения Vercel
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
@@ -19,21 +17,17 @@ const firebaseConfig = {
 
 // Функция для получения или инициализации App
 const initializeFirebaseApp = () => {
-    // Если приложение уже инициализировано, возвращаем его
+    // Предотвращаем повторную инициализацию
     if (getApps().length) {
         return getApp();
     }
-    // Иначе инициализируем новое
     return initializeApp(firebaseConfig);
 };
 
-// Инициализация App
 const app = initializeFirebaseApp();
 
 // Экспорт необходимых сервисов
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app); 
-
-// Также экспортируем саму константу, если она нужна
 export const firebaseApp = app;
