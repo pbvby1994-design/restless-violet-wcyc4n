9987686f6749bc7e3ed0b9c8a5a42205d412c987
Mikhail <pbvby1994@gmail.com>
2025-11-21 13:50:44 +0500

    Update index.js

--- webapp/pages/index.js
+++ webapp/pages/index.js
@@ -1,15 +1,21 @@
 // Файл: webapp/pages/index.js
-"use client"; // <-- КРИТИЧЕСКОЕ ИСПРАВЛЕНИЕ
+"use client"; // Оставляем, так как страница использует клиентский код
 
+import dynamic from 'next/dynamic';
 import Generator from '@/components/Generator';
-import Layout from '@/components/Layout';
 import MiniPlayer from '@/components/MiniPlayer';
+
+// ✅ КРИТИЧЕСКОЕ ИСПРАВЛЕНИЕ: Динамический импорт Layout с отключенным SSR.
+// Это гарантирует, что WebApp SDK внутри Layout.js будет инициализирован только в браузере.
+const DynamicLayout = dynamic(() => import('@/components/Layout'), { 
+    ssr: false 
+});
 
 export default function IndexPage() {
   return (
-    <Layout>
+    <DynamicLayout>
       <Generator />
       <MiniPlayer />
-    </Layout>
+    </DynamicLayout>
   );
 }
